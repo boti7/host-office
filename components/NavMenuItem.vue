5fa00dee0e48090f405a27574895aa0d36e6002b
Botond Horvath <hello@boti7.com>
2020-01-28 15:37:04 +0100

    Add components: Navbar, NavMenuItem

---
+++ components/NavMenuItem.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="menu-item">
+    <a :href="href" @click="$emit('click', href)">{{ label }}</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'NavMenuItem',
+    props: {
+      href: {
+        type: String,
+        default: '#'
+      },
+      label: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a {
+    display: block;
+    padding: 20px 10px;
+    color: #39424E;
+    font-size: 18px;
+    text-decoration: none;
+  }
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="container">
+    <div class="brand">
+      <a href="/">Host Office kft.</a>
+    </div>
+
+    <div class="menu">
+      <NavMenuItem href="#rolunk" label="Rólunk" />
+    </div>
+  </nav>
+</template>
+
+<script>
+  import NavMenuItem from "./NavMenuItem";
+  export default {
+    name: 'Navbar',
+    components: { NavMenuItem }
+  }
+</script>
+
+<style scoped>
+  nav {
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .brand a {
+    display: block;
+    padding: 20px 10px;
+    color: #39424E;
+    font-size: 24px;
+    text-decoration: none;
+  }
+</style>
