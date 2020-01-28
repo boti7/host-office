683b327cf5f2031e44a9c78d342ba2bd157e8e1f
Botond Horvath <hello@boti7.com>
2020-01-28 15:39:33 +0100

    Add component: Hero

---
+++ components/Hero.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="hero-wrapper">
+    <div class="hero-content">
+      <h1 class="title">Domain regisztrációs szolgáltatások</h1>
+
+      <div class="search-box"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hero'
+  }
+</script>
+
+<style lang="scss" scoped>
+  @import '@/assets/_variables.scss';
+
+  .hero-wrapper {
+    width: 100%;
+    height: calc(100vh - 70px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $hero-bg-color;
+  }
+
+  .hero-content {
+    max-width: 1024px;
+    text-align: center;
+  }
+
+  .title {
+    font-weight: 300;
+    font-size: 40px;
+    color: $hero-title-color;
+  }
+
+  .search-box {
+    width: calc(100% - 40px);
+    max-width: 700px;
+    min-height: 60px;
+    margin: 20px;
+    border-radius: 10px;
+    background-color: $bg-color-light;
+  }
+</style>
